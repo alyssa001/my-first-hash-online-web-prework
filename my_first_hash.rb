a7cd6be6169341ef4a125be59ae5e6d2d6fb6447
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-06-03 00:51:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,9 +4,9 @@ end
 
 
 def shipping_manifest
-  the_manifest = { "whale_bone_corsets" => 5, 
-                   "porcelain_vases" => 2,
-                   "oil_paintings" => 3
+  the_manifest = { "whale bone corsets" => 5, 
+                   "porcelain vases" => 2,
+                   "oil paintings" => 3
                 }
   return the_manifest
 end
@@ -26,6 +26,6 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest["muskets"] = 2 
-  shipping_manifest["gun_powder"] = 4
+  shipping_manifest["gun powder"] = 4
   shipping_manifest
 end
